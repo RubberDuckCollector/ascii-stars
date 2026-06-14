263012bfd949d0ffdd444f2e5343d41376923d04
RubberDuckCollector <eggsim49@gmail.com>
2026-06-14 12:44:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<!-- vim-markdown-toc GFM -->
+
+* [ASCII stars](#ascii-stars)
+* [Install](#install)
+* [Uninstall](#uninstall)
+* [Examples](#examples)
+* [Why?](#why)
+* [Inspiration](#inspiration)
+
+<!-- vim-markdown-toc -->
+
+# ASCII stars
+
+Program that can generate 
+
+# Install
+
+# Uninstall
+
+# Examples
+
+# Why?
+
+I nearly always prefer art that I have created myself as my backgrounds/PFPs
+
+# Inspiration
+
+- The ASCII art in [Exercise 067](https://codeberg.org/ziglings/exercises/src/branch/main/exercises/067_comptime2.zig) of [ziglings](https://ziglings.org/)
---
+++ main.py
@@ -0,0 +1,17 @@
+# TODO: make a program that creates space themed backgrounds/screensavers with ASCII art
+# STEP 1. random distribution of stars
+# STEP 2. speicify new astronomical bodies (such as galaxies / extra big stars)
+# STEP 3. accommodate for higher resolutions and make black holes / quasars etc
+# STEP 4. define solar systems and include them at medium range
+# STEP 5. define what terrestrial / gas giant planets could look like and
+# create zoomed-in graphics of them
+# STEP 6. define different galaxy types
+# STEP 7. define different nebula types
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
