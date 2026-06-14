64f24bd9087c4b4a646fe40be84284fbca8095d4
RubberDuckCollector <eggsim49@gmail.com>
2026-06-14 21:50:05 +0100

    rust version see terminal size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use terminal_size::{Height, Width, terminal_size};
+
 fn main() {
-    println!("Hello, world!");
+    let terminal_size = terminal_size();
+    if let Some((Width(w), Height(h))) = terminal_size {
+        println!("Your terminal is {} cols wide and {} lines tall", w, h);
+    } else {
+        println!("Unable to get terminal terminal_size");
+    }
 }
