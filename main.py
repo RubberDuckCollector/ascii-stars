b50be6e23853b6efa65dfdcdc69133339b49adf6
RubberDuckCollector <eggsim49@gmail.com>
2026-06-14 21:15:06 +0100

    moved my_modules to a python file instead of directory

--- main.py
+++ main.py
@@ -4,35 +4,12 @@ import readline
 import my_modules
 
 
-class Color:
-    Reset = "\033[0m"
-    Red = "\033[031m"
-    Green = "\033[32m"
-    Yellow = "\033[33m"
-    Blue = "\033[34m"
-    Magenta = "\033[35m"
-    Cyan = "\033[96m"
-    LightGray = "\033[37m"
-    DarkGray = "\033[90m"
-    LightRed = "\033[91m"
-    LightGreen = "\033[92m"
-    LightYellow = "\033[93m"
-    LightBlue = "\033[94m"
-    LightMagenta = "\033[95m"
-    LightCyan = "\033[96m"
-    White = "\033[97m"
-    Warn = "\033[93m"
-    Underline = "\033[4m"
-    Bold = "\033[1m"
-    Hidden = "\033[8m"
-    Blink = "\033[5m"
-    Dim = "\033[2m"
-    Reverse = "\033[7m"
-    BlackBg = "\033[40m"
-
-
 # TODO: make a program that creates space themed backgrounds/screensavers with ASCII art
 # STEP 1. random distribution of stars
+#   - star distribution should be proportional to terminal area (set by a hard-coded number)
+#   - there should be a vocabulary of symbols that the program can use to represent stars
+#       * to make it more interesting
+#       * the user/programmer should be able to specify more stylized / more realistic
 # STEP 2. specify new astronomical bodies (such as galaxies / extra big stars)
 # STEP 3. accommodate for higher resolutions and make black holes / quasars etc
 # STEP 4. define solar systems and include them at medium range
@@ -49,12 +26,12 @@ def main():
     print(terminal_size.lines)
 
     os.system("clear")
-    print(Color.BlackBg)
-    print(Color.Dim)
+    print(my_modules.Color.BlackBg)
+    print(my_modules.Color.Dim)
     for i in range(terminal_size.lines):
         for j in range(terminal_size.columns):
             print(f" ", end='')
-    print(Color.Reset, end='')
+    print(my_modules.Color.Reset, end='')
     input()  # locks the screen so you can review the output
 
 
