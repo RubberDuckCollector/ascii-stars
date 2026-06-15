89bb5922a8ea5c92c26204b2590f943d8531e081
Luna Long <eggsim49@gmail.com>
2026-06-15 01:11:26 +0100

    code comments

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,6 @@ mod terminal_management;
 fn main() {
     let terminal_size = terminal_management::get_terminal_size();
 
-    // let (w, h) = terminal_size
-    //     .map(|(Width(w), Height(h))| (w, h))
-    //     .unwrap_or((80, 24));
-
-    // if terminal_size.is_none() {
-    //     eprintln!("Could not detect a terminal size. Using fallback dimensions of 80x24")
-    // }
 
     println!("Terminal width: {}", terminal_size.width);
     println!("Terminal height: {}", terminal_size.height);
--- src/terminal_management.rs
+++ src/terminal_management.rs
@@ -1,5 +1,14 @@
 use terminal_size::{Height, Width, terminal_size};
 
+// ALTERNATE IMPLMENTATION OF RETRIEVING TERMINAL DIMENSIONS
+// let (w, h) = terminal_size
+//     .map(|(Width(w), Height(h))| (w, h))
+//     .unwrap_or((80, 24));
+//
+// if terminal_size.is_none() {
+//     eprintln!("Could not detect a terminal size. Using fallback dimensions of 80x24")
+// }
+
 pub struct TerminalSize {
     pub width: u16,
     pub height: u16,
@@ -14,7 +23,7 @@ impl TerminalSize {
 pub fn get_terminal_size() -> TerminalSize {
     let terminal_size = terminal_size(); // returns an Option
 
-    // this releases w and h into the current scope
+    // this destructs the Option and releases w and h into the current scope
     let (width, height) = terminal_size
         .map(|(Width(w), Height(h))| (w, h))
         .unwrap_or((80, 24));
