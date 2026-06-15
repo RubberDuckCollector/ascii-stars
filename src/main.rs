bb3acd0d2d03f6e45d27fe6858e92e9c90a73d66
RubberDuckCollector <eggsim49@gmail.com>
2026-06-15 10:17:25 +0100

    changed `clear_screen()` to a system command for 'clear'

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,8 @@ use std::io::Write;
 
 mod terminal_management;
 
-//  import os
-//  import random
-//  import readline
-//  import subprocess
-//  import my_modules
-//
-//
-//  def main():
-//      terminal_size = os.get_terminal_size()
-//      print(terminal_size)
-//      print(terminal_size.columns)
-//      print(terminal_size.lines)
-//
-//      subprocess.run("clear")
-//      print(my_modules.Color.BlackBg)
-//      print(my_modules.Color.Dim)
-//      for i in range(terminal_size.lines):
-//          for j in range(terminal_size.columns):
-//              print(f" ", end='')
-//      print(my_modules.Color.Reset, end='')
-//      input()  # locks the screen so you can review the output
-//
-
-//  if __name__ == "__main__":
-//      main()
-
 fn clear_screen() {
-    // Source - https://stackoverflow.com/a/34837038
-    // Posted by minghan, modified by community. See post 'Timeline' for change history
-    // Retrieved 2026-06-15, License - CC BY-SA 4.0
-
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    std::process::Command::new("clear");
 }
 
 fn main() {
