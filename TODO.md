de7d24f4ed418f1385460e9d123d003966fe5f78
RubberDuckCollector <eggsim49@gmail.com>
2026-06-15 13:18:00 +0100

    changed `TODO.md` and implemented random selection of specified symbols to be written to the screen

--- TODO.md
+++ TODO.md
@@ -1,17 +1,20 @@
-<!-- TODO: make a program that creates space themed backgrounds/screensavers with ASCII art -->
-<!-- STEP 1. random distribution of stars -->
-<!--   - star distribution should be proportional to terminal area (set by a hard-coded number) -->
-<!--   - there should be a vocabulary of symbols that the program can use to represent stars -->
-<!--       * to make it more interesting -->
-<!--       * the user/programmer should be able to specify more stylized / more realistic -->
-<!-- STEP 2. specify new astronomical bodies (such as galaxies / extra big stars) -->
-<!-- STEP 3. accommodate for higher resolutions and make black holes / quasars etc -->
-<!-- STEP 4. define solar systems and include them at medium range -->
-<!-- STEP 5. define what terrestrial / gas giant planets could look like and -->
-<!-- create zoomed-in graphics of them -->
-<!-- STEP 6. define different galaxy types -->
-<!-- STEP 7. define different nebula types -->
+TODO: make a program that creates space themed backgrounds/screensavers with ASCII art
+STEP 1. random distribution of stars
+  - star distribution should be proportional to terminal area (set by a hard-coded number)
+  - there should be a vocabulary of symbols that the program can use to represent stars
+      * to make it more interesting
+      * the user/programmer should be able to specify more stylized / more realistic
+STEP 2. specify new astronomical bodies (such as galaxies / extra big stars)
+STEP 3. accommodate for higher resolutions and make black holes / quasars etc
+STEP 4. define solar systems and include them at medium range
+STEP 5. define what terrestrial / gas giant planets could look like and
+create zoomed-in graphics of them
+STEP 6. define different galaxy types
+STEP 7. define different nebula types
 
+TODO: get a 2d array the size of the terminal
+do passes of star arrangement algorithms on the 2d array to progressively add symbols to the image
+need priority queue for which celestial objects need to be in fg/bg? or just the correct order in which to run the algorithms?
 
 
 KEY: 🟢 completed
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,23 @@ fn clear_screen() {
     std::process::Command::new("clear");
 }
 
+#[derive(Debug)]
+struct Stars {
+    dot: String,
+    asterisk: String,
+    o: String,
+}
+
+impl Stars {
+    fn instantiate_stars(param_dot: String, param_asterisk: String, param_o: String) -> Stars {
+        Stars {
+            dot: param_dot,
+            asterisk: param_asterisk,
+            o: param_o,
+        }
+    }
+}
+
 fn main() {
     let terminal_size = terminal_management::get_terminal_size();
 
@@ -16,11 +33,47 @@ fn main() {
     println!("Terminal lines: {}", terminal_size.lines);
     println!("Terminal area: {}", terminal_size.get_area());
 
+    let mut rng = rand::rng(); // get rng object
+
+    // let mut dot_symbol = String::new();
+    // let mut asterisk_symbol = String::new();
+    // let mut o_symbol = String::new();
+
+    println!("About to generate screen. Press enter to continue.");
+
+    let dot_symbol = ".".to_string();
+    let asterisk_symbol = "*".to_string();
+    let o_symbol = "o".to_string();
+
+    let mut star_symbols = vec![dot_symbol, asterisk_symbol, o_symbol];
+
+    let my_stars = Stars::instantiate_stars(
+        star_symbols[0].clone(),
+        star_symbols[1].clone(),
+        star_symbols[2].clone(),
+    );
+    println!("my_stars: {:#?}", my_stars);
+
+    println!("About to generate screen. Press enter to continue.");
+    std::io::stdout().flush().unwrap(); // makes the text print immediately
+
+    let mut msg = "".to_string();
+    std::io::stdin().read_line(&mut msg).unwrap(); // input to pause execution after the colored text is done (can verify the screen works)
+
     clear_screen();
 
     for _ in 1..=terminal_size.lines {
         for _ in 1..=terminal_size.columns {
-            print!("{}", Style::new().on(Color::Black).paint(" "));
+            let is_filled_in_with_star: u8 = rand::random_range(0..=2);
+            match is_filled_in_with_star {
+                0 => match star_symbols.choose(&mut rand::rng()) {
+                    Some(i) => print!("{}", Style::new().on(Color::Black).paint(i)),
+                    None => panic!("Error while deciding what star should go in the space."),
+                },
+                1 => print!("{}", Style::new().on(Color::Black).paint(" ")),
+                2 => print!("{}", Style::new().on(Color::Black).paint(" ")),
+                _ => panic!("Error while matching whether a space is filled in with a star."),
+            }
         }
     }
     std::io::stdout().flush().unwrap(); // makes the text print immediately
@@ -28,8 +81,6 @@ fn main() {
     let mut msg = "".to_string();
     std::io::stdin().read_line(&mut msg).unwrap(); // input to pause execution after the colored text is done (can verify the screen works)
 
-    let mut rng = rand::rng(); // get rng object
-    let x: f32 = rng.random();
-
-    println!("rand f32 0.0..=1.0: {}", x);
+    // let x: f32 = rng.random();
+    // println!("rand f32 0.0..=1.0: {}", x);
 }
